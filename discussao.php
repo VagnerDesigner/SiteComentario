5f208bb34e43dfebfe2c07f83bf7063c297a9d29
Vagner Designer <designercriativo2609@gmail.com>
2019-10-22 21:34:53 -0300

    Primeiro commit

---
+++ discussao.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Sistema de Cometários</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="CSS/discussao.css">
+	
+</head>
+<body>
+
+	<nav>
+			<ul>
+				<li><a href="index.php">Início</a></li>
+				<li><a href="">Entrar</a></li>
+			</ul>
+	</nav>
+
+<div id="largurapagina">
+
+<section id="conteudo1">
+
+	<h1>CIÊNCIA DE DADOS: O QUE É, CONCEITO E DEFINIÇÃO</h1>
+
+	<img src="imagens/datascience.png">
+
+	<p class="Texto">Por mais que você ainda não tenha se atentado a esse fato, grande partes dos negócios já faz parte do mercado de dados e informações.
+
+A expectativa é que em 2017 sejamos cerca de 5 bilhões de mobile phones conectados, que serão responsáveis por produzir um fluxo gigantesco e constante de informação digital.<br>
+
+A estimativa afirma que cerca de 90% de todos os dados armazenados em todo o mundo foram produzidos somente nos dois últimos anos e seus rastros continuam se multiplicando a cada ano que passa.
+Quando analisamos essas informações parece tudo muito assustador e impossível de ser controlado, certo? – Errado! – A ciência de dados está cada vez mais consistente no mercado para que possamos acompanhar esse crescimento sem tropeços no futuro. E sabe qual o resultado dessa evolução toda de informações? – O Big  Bang do Dataverse. – Hpa afirmações que essa será a próxima onda.<br>
+
+Portanto, inevitavelmente grande parte das empresas fazem parte desse ecossistema de dados, e sem um acompanhamento e analises cuidadosas em um curto prazo será quase impossível tomar decisões inteligentes a favor de bons resultados. É importante destacar que os dados permitem que as empresas e organizações entendam melhor seus clientes e consumidores e possam aprimorar cada vez mais os seus processos.<br>
+
+Diante de todos esses fatos, a Data Science ou Ciência de Dados tem sido muito mais relevante em noticias sobre indústria e negócios, e não é pra menos, pois trata-se de uma disciplina acadêmica e profissão totalmente em alta no mercado.<br>
+
+Para não haver confusão, é importante destacar que a ciência de dados é diferente das analises estatísticas e também da ciência da computação com relação ao seu método aplicado aos dados coletados, usando princípios científicos.<br>
+
+A principal razão disso é o chamado Big Data, que é responsável por demandar o uso de diferentes tecnologias com relação à analise estatística. Em outras palavras, profissionais de estatísticas existentes a anos no mercado não conseguiriam fazer uma analise profunda dos dados em massa quase que em tempo real, que é o que ocorre nos dias de hoje nas grandes empresas.<br>
+
+Nesse caso, a ciência de dados está buscando descobrir conhecimento demandável a partir de uma quantidade grande e pesada de dados que podem ser usadas para tomar decisões e fazer previsões, e não simplesmente a interpretação de números. É por esse motivo que o mercado está cada vez mais em busca de cientistas de dados, que possuem requisitos como o conhecimento em engenharia e o Ph.D também.
+
+</p>
+	<h2>Deixe o seu comentário</h2>
+
+<form method="POST">
+	<img src="imagens/perfil.png">
+	<textarea name="texto" placeholder="Participe da discursão"></textarea>
+	<input type="submit" value="PUBLICAR COMENTÁRIO">
+</form>
+
+
+<div class="area-comentario">
+	<img src="imagens/perfil.png">
+	<h3>Nome da Pessoa</h3>
+	<h4>horário e data <a href="">Excluir</a></h4>
+	<p>Comentário desta pessoa</p>
+</div>
+<div class="area-comentario">
+	<img src="imagens/perfil.png">
+	<h3>Nome da Pessoa 2</h3>
+	<h4>horário e data <a href="">Excluir</a></h4>
+	<p>Comentário desta pessoa</p>
+</div>
+<div class="area-comentario">
+	<img src="imagens/perfil.png">
+	<h3>Nome da Pessoa 3</h3>
+	<h4>horário e data <a href="">Excluir</a></h4>
+	<p>Comentário desta pessoa</p>
+</div>
+<div class="area-comentario">
+	<img src="imagens/perfil.png">
+	<h3>Nome da Pessoa 4</h3>
+	<h4>horário e data <a href="">Excluir</a></h4>
+	<p>Comentário desta pessoa</p>
+</div>
+<div class="area-comentario">
+	<img src="imagens/perfil.png">
+	<h3>Nome da Pessoa 5</h3>
+	<h4>horário e data <a href="">Excluir</a></h4>
+	<p>Comentário desta pessoa</p>
+</div>
+<div class="area-comentario">
+	<img src="imagens/perfil.png">
+	<h3>Nome da Pessoa6</h3>
+	<h4>horário e data <a href="">Excluir</a></h4>
+	<p>Comentário desta pessoa</p>
+</div>
+
+</section>
+
+<section id="Conteudo2">
+	<div>
+		<img src="imagens/eva.jpg">
+		<p>Eva Green é uma atriz e modelo francesa, conhecida por interpretar a personagem Vanessa Ives na série Penny Dreadful. Eva vem sendo considerada como uma das atrizes mais talentosas e promissoras de sua geração, sendo inclusa constantemente em listas de melhores atrizes de todos os tempos.</p>
+	</div>
+</section>
+
+<section id="Conteudo3">
+	<div>
+		<h5>Eva Green</h5>
+		<p>Eva Green é uma atriz e modelo francesa, conhecida por interpretar a personagem Vanessa Ives na série Penny Dreadful. Eva vem sendo considerada como uma das atrizes mais talentosas e promissoras de sua geração, sendo inclusa constantemente em listas de melhores atrizes de todos os tempos.</p>
+	</div>
+</section>
+
+</div>
+	
+</body>	
+</html>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="CSS/style.css">
+	<title>Sistema de Comentário</title>
+</head>
+<body>
+
+	<!-- Criando nosso meno de navegação !-->
+
+	<nav>
+			<ul>
+				<li><a href="discussao.php">Dicussão</a></li>
+				<li><a href="">Entrar</a></li>
+			</ul>
+	</nav>
+
+		<h1><p class="Texto2">Embora não seja claro onde realmente começou a história do cinema, a primeira exibição de um filme de curta duração aconteceu no Salão Grand Café, em Paris, a 28 de dezembro de 1895. Nesta data, os Irmãos Lumière fizeram uma apresentação pública dos produtos de seu invento ao qual chamaram Cinematógrafo.</p></h1>
+
+
+		
+
+
+
+	
+</body>
+</html>
+
+
+
+
